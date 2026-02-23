b987a314b1a33e830740ba60e9de921cecf3ad11
Kier Davis <me@kierdavis.com>
2026-02-23 03:13:10 +0000

    demo program: introduce application logic

--- firmware/underglow-fw/src/main.rs
+++ firmware/underglow-fw/src/main.rs
@@ -4,9 +4,31 @@
 use cortex_m_rt::entry;
 use panic_halt as _; // you can put a breakpoint on `rust_begin_unwind` to catch panics
 
+#[derive(Clone, Copy)]
+enum Colour {
+  Red,
+  Green,
+  Blue,
+}
+
 #[entry]
 fn main() -> ! {
   loop {
-    // your code goes here
+    Colour::Red.show();
+    delay_one_second();
+    Colour::Green.show();
+    delay_one_second();
+    Colour::Blue.show();
+    delay_one_second();
   }
+}
+
+impl Colour {
+  fn show(self) {
+    todo!()
+  }
+}
+
+fn delay_one_second() {
+  todo!()
 }
