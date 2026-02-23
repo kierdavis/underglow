bf711139ed267838113cb5a748c0e4f360d833c5
Kier Davis <me@kierdavis.com>
2026-02-23 03:15:03 +0000

    demo program: introduce mapping of colours to GPIO pin states

--- firmware/underglow-fw/src/main.rs
+++ firmware/underglow-fw/src/main.rs
@@ -11,6 +11,13 @@ enum Colour {
   Blue,
 }
 
+#[derive(Clone, Copy)]
+enum GPIOPin {
+  Red,
+  Green,
+  Blue,
+}
+
 #[entry]
 fn main() -> ! {
   loop {
@@ -25,6 +32,28 @@ fn main() -> ! {
 
 impl Colour {
   fn show(self) {
+    match self {
+      Self::Red => {
+        GPIOPin::Red.set_output_state(true);
+        GPIOPin::Green.set_output_state(false);
+        GPIOPin::Blue.set_output_state(false);
+      }
+      Self::Green => {
+        GPIOPin::Red.set_output_state(false);
+        GPIOPin::Green.set_output_state(true);
+        GPIOPin::Blue.set_output_state(false);
+      }
+      Self::Blue => {
+        GPIOPin::Red.set_output_state(false);
+        GPIOPin::Green.set_output_state(false);
+        GPIOPin::Blue.set_output_state(true);
+      }
+    }
+  }
+}
+
+impl GPIOPin {
+  fn set_output_state(self, state: bool) {
     todo!()
   }
 }
