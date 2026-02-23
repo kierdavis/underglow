64268837c26490d4cdf6f4324fc2063ddac89df0
Kier Davis <me@kierdavis.com>
2026-02-23 03:00:52 +0000

    tidy imports

--- firmware/underglow-fw/src/main.rs
+++ firmware/underglow-fw/src/main.rs
@@ -1,9 +1,8 @@
 #![no_std]
 #![no_main]
 
-use panic_halt as _; // you can put a breakpoint on `rust_begin_unwind` to catch panics
-
 use cortex_m_rt::entry;
+use panic_halt as _; // you can put a breakpoint on `rust_begin_unwind` to catch panics
 
 #[entry]
 fn main() -> ! {
